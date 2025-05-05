da5be23c5470cbc9c1770f0f7452f69e6b1dd984
Nikhil <168549715+NikhilPatil2727@users.noreply.github.com>
2025-05-05 18:08:50 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     prism.highlightAll();
   }, []); // Runs only once on mount
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", { code });
+    const response = await axios.post("https://ai-powerd-code-reviewer-backend.onrender.com", { code });
     setReivew(response.data);
   }
 
